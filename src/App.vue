fd20bbdb52732f3c1b9859657807207ad037dee4
luismartinezs <martinez.s.luis@gmail.com>
2021-04-29 17:35:51 +0200

    Named slots

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="container mx-auto px-6">
+    <BlogLayout>
+      <template #header>
+        <h1>My header</h1>
+      </template>
+      <p>My main content</p>
+      <template #sidebar>
+        <h2>My sidebar</h2>
+      </template>
+    </BlogLayout>
     <AppIcon class="hidden" />
     <div class="flex">
       <BaseButton />
@@ -12,11 +21,12 @@
 
 <script>
 import BaseButton from "@/components/BaseButton.vue";
+import BlogLayout from "@/components/BlogLayout.vue";
 import AppIcon from "@/components/AppIcon.vue";
 
 export default {
   name: "App",
-  components: { BaseButton, AppIcon },
+  components: { BaseButton, AppIcon, BlogLayout },
 };
 </script>
 
---
+++ src/components/BlogLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+  <aside>
+    <slot name="sidebar"></slot>
+  </aside>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
